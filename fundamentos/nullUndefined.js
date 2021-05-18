6e2af8568831bca8c04cd12309bc58f5ee12da82
GabrielGM01 <gabrielgusmaomoreno@gmail.com>
2021-05-18 15:27:15 -0300

    fundamentos

---
+++ fundamentos/nullUndefined.js
@@ -0,0 +1,12 @@
+let valor
+console.log(valor)
+
+valor = null
+console.log(valor)
+// console.log(valor.toString()) // Erro
+
+const produto = {}
+console.log(produto.preco)
+
+produto.preco = 3.50
+console.log(produto)
