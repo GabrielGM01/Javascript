4a40eef27b40a2b4517dc987da3977b6d9ae0a9a
GabrielGM01 <gabrielgusmaomoreno@gmail.com>
2021-05-18 13:46:42 -0300

    fundamentos

---
+++ fundamentos/booleano.js
@@ -0,0 +1,18 @@
+let isAtivo = false
+console.log(isAtivo)
+
+isAtivo = true
+console.log(isAtivo)
+
+isAtivo = 1
+console.log(!!isAtivo)
+
+console.log('os verdadeiros...')
+console.log(!!3.14)
+console.log(!!-1)
+console.log(!!' ')
+console.log('Os falsos')
+console.log(!!0)
+console.log(!!'')
+console.log(!!null)
+
