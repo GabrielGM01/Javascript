2a5d5825b9c543f66a8e5684aa738d2de9b58c57
GabrielGM01 <gabrielgusmaomoreno@gmail.com>
2021-05-18 17:01:28 -0300

    fundamentos

---
+++ fundamentos/funcao2.js
@@ -0,0 +1,15 @@
+const imprimirSoma = function (a, b) {
+  console.log(a + b)
+}
+
+imprimirSoma(2, 3)
+
+const soma = (a, b) => {
+  return a + b
+}
+
+console.log(soma(20, 10))
+
+const subtracao = (a, b) => a - b
+
+console.log(subtracao(50, 10))
