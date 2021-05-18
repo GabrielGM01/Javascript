a5914c108057df5308933cd569a995cdb085d2c4
GabrielGM01 <gabrielgusmaomoreno@gmail.com>
2021-05-18 11:51:59 -0300

    fundamentos

---
+++ fundamentos/string.js
@@ -0,0 +1,11 @@
+const escola = "Monica"
+
+console.log(escola.charAt(4))
+console.log(escola.charAt(5))
+console.log(escola.charCodeAt(3))
+console.log(escola.indexOf('c'))
+
+console.log('Nome: '.concat(escola).concat("!"))
+console.log(escola.replace('M', 'P'))
+console.log('Ana,Pedro,Luan'.split(','))
+console.log('Escola Turma da ' + escola + '!')
