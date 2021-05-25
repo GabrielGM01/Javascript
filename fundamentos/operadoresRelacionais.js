a4a6bbd5e3cd5f3bf22b796e25587bf088aead90
GabrielGM01 <gabrielgusmaomoreno@gmail.com>
2021-05-24 21:55:52 -0300

    fundamentos

---
+++ fundamentos/operadoresRelacionais.js
@@ -0,0 +1,12 @@
+console.log('01)', '1' == 1);
+console.log('01)', '1' === 1);
+console.log('03)', '3' != 3);
+console.log('04)', '3' !== 3);
+
+console.log('05)', 3 <= 2);
+
+const d1 = new Date(0);
+const d2 = new Date(0);
+console.log('-------------------');
+console.log('6)', d1 == d2);
+console.log('7)', d1 === d2);
