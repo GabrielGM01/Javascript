bbbcc26895f7c82dbd5e35f40c26c01a94516cb5
GabrielGM01 <gabrielgusmaomoreno@gmail.com>
2021-05-25 14:48:22 -0300

    Estrutura de dados

---
+++ EstruturasDeControles/If.js
@@ -0,0 +1,13 @@
+function soBoanoticia(nota) {
+  if (nota >= 7) {
+    console.log(`Aprovado com ${nota}`);
+  }
+}
+soBoanoticia(8.1);
+
+function seForVerdadeEuFalo(valor) {
+  if (valor) {
+    console.log(`È verdade... ${valor}`);
+  }
+}
+seForVerdadeEuFalo(20);
