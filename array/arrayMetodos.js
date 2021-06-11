12c434bf9f4c9d7550a9ff28d0b0a8552be2fd8a
GabrielGM01 <gabrielgusmaomoreno@gmail.com>
2021-06-11 12:13:05 -0300

    array

---
+++ array/arrayMetodos.js
@@ -0,0 +1,16 @@
+const pilotos = ['João', 'Alfonso', 'Moises', 'Massa'];
+
+pilotos.pop(); //remove ultimo elemento do array
+pilotos.push('Araan');
+
+pilotos.shift(); //remove o primeiro!
+
+pilotos.unshift('Gabriel'); //adiciona o primeiro
+
+console.log(pilotos);
+
+const algunsPilotos = pilotos.slice(2) //novo array
+console.log(algunsPilotos);
+
+
+
