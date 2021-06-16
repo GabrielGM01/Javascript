330047049aea6355fe8717b2db83f2468c120549
GabrielGM01 <gabrielgusmaomoreno@gmail.com>
2021-06-16 15:52:31 -0300

    node

---
+++ Node/PastaA/PastaB/acessarModulo.js
@@ -0,0 +1,11 @@
+const moduloA = require('../../ModuloA');
+// const moduloA = require('C:\curso\Javascript\Node\ModuloA.js');
+console.log(moduloA.bemVindo);
+const c = require('./pastaC');
+console.log(c.ola2);
+
+/*const http = require('http');
+http.createServer((req, res) => {
+  res.write();
+  res.end();
+}).listen(8080);*/
